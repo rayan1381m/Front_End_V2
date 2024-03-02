83f7c42d94d08e309c46bcdd06edc355259ce94d
Git-Test <rayan13811381@gmail.com>
2024-03-03 02:36:48 +0330

    inam env ezaf shod

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import './App.css'
 import Login from './components/login'
 
+const BASE_URL = import.meta.env.VITE_Backend_BASE_URL
 function App() {
 
   return (
--- src/components/login.jsx
+++ src/components/login.jsx
@@ -1,16 +1,46 @@
+//https://www.youtube.com/watch?v=bMRrSqWFKqM
+//https://www.youtube.com/watch?v=Gl-vOU7ZU9A
+//https://www.youtube.com/watch?v=PmPkAAu_QF4
+
 import "./login.css";
 import { useState } from "react";
+import axios from "axios";
 
 function Login() {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+
+    try {
+        const response = await axios.post("http://localhost:3000/users/login", {
+        username,
+        password,
+      });
+
+      if (response.data.message === "Login successful") {
+        const isAdmin = response.data.user.is_admin;
+        if (isAdmin) {
+          console.log("Redirecting to admin panel");
+        } else {
+          console.log("Redirecting to user panel");
+        }
+      } else {
+        setError("Invalid username or password");
+      }
+    } catch (error) {
+      setError("Error logging in");
+    }
+  };
 
   return (
     <div className="container">
       <div className="header">
         <h1>JEDI</h1>
       </div>
-      <form className="form-body">
+      <form className="form-body" onSubmit={handleLogin}>
         <div className="email-div">
           <label htmlFor="email">Username</label>
           <input
@@ -35,6 +65,7 @@ function Login() {
             placeholder="password"
           />
         </div>
+        {error && <p className="error">{error}</p>}
         <div>
           <button type="submit" className="login-btn">
             LOG IN
