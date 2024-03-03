b25647cfe171e1cb5e409fa0719e5592d3341ab8
Git-Test <rayan13811381@gmail.com>
2024-03-03 12:56:24 +0330

    create signup form

--- src/components/login.jsx
+++ src/components/login.jsx
@@ -6,49 +6,28 @@ import "./login.css";
 import { useState } from "react";
 import axios from "axios";
 
+const users = [
+  {
+    name : "Rayan",
+    isAdmin: true,
+    password: "password123"
+  }
+]
+
+
 function Login() {
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
-  const [error, setError] = useState("");
-
-  const handleLogin = async (e) => {
-    e.preventDefault();
-
-    try {
-        const response = await axios.post("http://localhost:3000/users/login", {
-        username,
-        password,
-      });
-
-      if (response.data.message === "Login successful") {
-        const isAdmin = response.data.user.is_admin;
-        if (isAdmin) {
-          console.log("Redirecting to admin panel");
-        } else {
-          console.log("Redirecting to user panel");
-        }
-      } else {
-        setError("Invalid username or password");
-      }
-    } catch (error) {
-      setError("Error logging in");
-    }
-  };
-
   return (
     <div className="container">
       <div className="header">
         <h1>JEDI</h1>
       </div>
-      <form className="form-body" onSubmit={handleLogin}>
+      <form className="form-body" >
         <div className="email-div">
           <label htmlFor="email">Username</label>
           <input
             type="text"
             name="email"
             id="email"
-            value={username}
-            onChange={(e) => setUsername(e.target.value)}
             placeholder="username"
             required
           />
@@ -59,14 +38,9 @@ function Login() {
             type="password"
             name="password"
             id="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
             required
             placeholder="password"
           />
-        </div>
-        {error && <p className="error">{error}</p>}
-        <div>
           <button type="submit" className="login-btn">
             LOG IN
           </button>
